7a9f707b2d7adb1a746b8253e7e7389c93a48fc5
Tanner <troxs690@gmail.com>
2022-02-11 12:09:32 -0600

    done

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  const fetcher = fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(kol => renderBooks(kol))
+    return fetcher
 }
+     
+    
+
+    
+  
+
+
+  
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
